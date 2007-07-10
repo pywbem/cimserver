d720b914317584e35d59b0e4306b734207689585
Bart Whiteley <bwhiteley@gmail.com>
2007-07-10 23:41:00 +0000

    added grammer.  mostly recognizes cimv2.14, but stringValues need work.
    git-svn-id: https://svn.code.sf.net/p/pywbem/code/cimserver/trunk@259 84f79d66-d411-0410-a85a-a3183736c6e8

--- mofcomp.py
+++ mofcomp.py
@@ -19,59 +19,54 @@
 
 # Author: Bart Whiteley <bwhiteley@suse.de>
 
+import sys
+import os
 import ply.lex as lex
+import ply.yacc as yacc
 
-tokens = (
-        'ANY', 
-        'AS',
-        'ASSOCIATION',
-        'CLASS',
-        'DISABLEOVERRIDE',
-        'DT_BOOL',
-        'DT_CHAR16',
-        'DT_DATETIME',
-        'DT_REAL32',
-        'DT_REAL64',
-        'DT_SINT16',
-        'DT_SINT32',
-        'DT_SINT64',
-        'DT_SINT8',
-        'DT_STR',
-        'DT_UINT16',
-        'DT_UINT32',
-        'DT_UINT64',
-        'DT_UINT8',
-        'ENABLEOVERRIDE',
-        'FALSE',
-        'FLAVOR',
-        'INDICATION',
-        'INSTANCE',
-        'METHOD',
-        'NULL',
-        'OF',
-        'PARAMETER',
-        'PRAGMA',
-        'PROPERTY',
-        'QUALIFIER',
-        'REF',
-        'REFERENCE',
-        'RESTRICTED',
-        'SCHEMA',
-        'SCOPE',
-        'TOSUBCLASS',
-        'TRANSLATABLE',
-        'TRUE',
-        'LPAREN',
-        'RPAREN',
-        'LBRACE',
-        'RBRACE',
-        'SEMICOLON',
-        'LBRACK',
-        'RBRACK',
-        'COMMA',
-        'DOLLAR',
-        'COLON',
-        'EQUALS',
+reserved = {
+    'any':'ANY',
+    'as':'AS',
+    'association':'ASSOCIATION',
+    'class':'CLASS',
+    'disableoverride':'DISABLEOVERRIDE',
+    'boolean':'DT_BOOL',
+    'char16':'DT_CHAR16',
+    'datetime':'DT_DATETIME',
+    'real32':'DT_REAL32',
+    'real64':'DT_REAL64',
+    'sint16':'DT_SINT16',
+    'sint32':'DT_SINT32',
+    'sint64':'DT_SINT64',
+    'sint8':'DT_SINT8',
+    'string':'DT_STR',
+    'uint16':'DT_UINT16',
+    'uint32':'DT_UINT32',
+    'uint64':'DT_UINT64',
+    'uint8':'DT_UINT8',
+    'enableoverride':'ENABLEOVERRIDE',
+    'false':'FALSE',
+    'flavor':'FLAVOR',
+    'indication':'INDICATION',
+    'instance':'INSTANCE',
+    'method':'METHOD',
+    'null':'NULL',
+    'of':'OF',
+    'parameter':'PARAMETER',
+    '#pragma':'PRAGMA',
+    'property':'PROPERTY',
+    'qualifier':'QUALIFIER',
+    'ref':'REF',
+    'reference':'REFERENCE',
+    'restricted':'RESTRICTED',
+    'schema':'SCHEMA',
+    'scope':'SCOPE',
+    'tosubclass':'TOSUBCLASS',
+    'translatable':'TRANSLATABLE',
+    'true':'TRUE',
+    }
+
+tokens = reserved.values() + [
         'IDENTIFIER',
         'stringValue',
         'floatValue',
@@ -80,7 +75,7 @@ tokens = (
         'octalValue',
         'decimalValue',
         'hexValue',
-    )
+    ]
 
 
 # UTF-8 (from Unicode 4.0.0 standard):
@@ -112,11 +107,13 @@ def t_COMMENT(t):
     r'//.*'
     pass
 
-t_IDENTIFIER = r'([a-zA-Z_]|{utf8Char})([0-9a-zA-Z_]|{utf8Char})*'
+def t_MCOMMENT(t):
+    r' /\*(.|\n)*?\*/'
+    t.lineno += t.value.count('\n')
 
 t_binaryValue = r'[+-]?[01]+[bB]'
-t_octalValue = r'[+-]?"0"[0-7]+'
-t_decimalValue = r'[+-]?([1-9][0-9]*|"0")'
+t_octalValue = r'[+-]?0[0-7]+'
+t_decimalValue = r'[+-]?([1-9][0-9]*|0)'
 t_hexValue = r'[+-]?"0"[xX][0-9a-fA-F]+'
 t_floatValue = r'[+-]?[0-9]*"."[0-9]+([eE][+-]?[0-9]+)?'
 
@@ -127,82 +124,397 @@ cChar = r"[^'\\\n\r]|{escapeSequence}"
 sChar = r'[^"\\\n\r]|{escapeSequence}'
 charValue = r"\'{cChar}\'"
 #t_stringValue = r'\"{sChar}*\"'
-ws = r'[ \t]+'
 #t_stringValue = r'1*("*{sChar}")'
 t_stringValue = r'"(\\"|[^"])*"'
 
-def t_CLASS(t):
-    r'class'
-    return t
+literals = '(){};[],$:='
 
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
-t_SEMICOLON = r'\;'
-t_LBRACK = r'\['
-t_RBRACK = r'\]'
-t_COMMA = r'\,'
-t_DOLLAR = r'\$'
-t_COLON = r'\:'
-t_EQUALS = r'\='
-
-t_TRUE = r'true'
-t_FALSE = r'false'
-
-t_ANY = r'any'
-t_AS = r'as'
-t_ASSOCIATION = r'association'
-#t_CLASS = r'class'
-t_DISABLEOVERRIDE = r'disableoverride'
-t_DT_BOOL = r'boolean'
-t_DT_CHAR16 = r'char16'
-t_DT_DATETIME = r'datetime'
-t_DT_REAL32 = r'real32'
-t_DT_REAL64 = r'real64'
-t_DT_SINT16 = r'sint16'
-t_DT_SINT32 = r'sint32'
-t_DT_SINT64 = r'sint64'
-t_DT_SINT8 = r'sint8'
-t_DT_STR = r'string'
-t_DT_UINT16 = r'uint16'
-t_DT_UINT32 = r'uint32'
-t_DT_UINT64 = r'uint64'
-t_DT_UINT8 = r'uint8'
-t_ENABLEOVERRIDE = r'enableoverride'
-t_FLAVOR = r'flavor'
-t_INDICATION = r'indication'
-t_INSTANCE = r'instance'
-t_METHOD = r'method'
-t_NULL = r'null'
-t_OF = r'of'
-t_PARAMETER = r'parameter'
 t_PRAGMA = r'\#pragma'
-t_PROPERTY = r'property'
-t_QUALIFIER = r'qualifier'
-t_REF = r'ref'
-t_REFERENCE = r'reference'
-t_RESTRICTED = r'restricted'
-t_SCHEMA = r'schema'
-t_SCOPE = r'scope'
-t_TOSUBCLASS = r'tosubclass'
-t_TRANSLATABLE = r'translatable'
+
+def t_IDENTIFIER(t):
+    r'([a-zA-Z_]|{utf8Char})([0-9a-zA-Z_]|{utf8Char})*'
+    t.type = reserved.get(t.value.lower(),'IDENTIFIER') # check for reserved word
+    return t
 
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-t_ignore = ' \t'
+t_ignore = ' \r\t'
 
 # Error handling rule
 def t_error(t):
-    print "Illegal character '%s'" % t.value[0]
+    msg = "Illegal character '%s' " % t.value[0]
+    msg+= "Line %d, col %d" % (t.lineno, find_column(mof, t))
+    print msg
     t.lexer.skip(1)
 
-# Build the lexer
-lex.lex(debug=1)
+def p_mofSpecification(p):
+    """mofSpecification : mofProductionList"""
+
+def p_mofProductionList(p):
+    """mofProductionList : empty
+                           | mofProductionList mofProduction
+                           """
+
+def p_mofProduction(p):
+    """mofProduction : compilerDirective
+                      | classDeclaration
+                      | assocDeclaration
+                      | indicDeclaration
+                      | qualifierDeclaration
+                      | instanceDeclaration
+                      """
+
+def p_compilerDirective(p): 
+    """compilerDirective : PRAGMA pragmaName '(' pragmaParameter ')'"""
+
+def p_pragmaName(p):
+    """pragmaName : identifier"""
+
+def p_pragmaParameter(p):
+    """pragmaParameter : stringValue"""
+
+def p_classDeclaration(p):
+    """classDeclaration : CLASS className '{' classFeatureList '}' ';'
+                        | CLASS className superClass '{' classFeatureList '}' ';'
+                        | CLASS className alias '{' classFeatureList '}' ';'
+                        | CLASS className alias superClass '{' classFeatureList '}' ';'
+                        | qualifierList CLASS className '{' classFeatureList '}' ';'
+                        | qualifierList CLASS className superClass '{' classFeatureList '}' ';'
+                        | qualifierList CLASS className alias '{' classFeatureList '}' ';'
+                        | qualifierList CLASS className alias superClass '{' classFeatureList '}' ';'
+                        """
+
+def p_classFeatureList(p):
+    """classFeatureList : empty
+                        | classFeatureList classFeature
+                        """
+
+def p_assocDeclaration(p):
+    """assocDeclaration : '[' ASSOCIATION qualifierListEmpty ']' CLASS className '{' associationFeatureList '}' ';'
+                        | '[' ASSOCIATION qualifierListEmpty ']' CLASS className superClass '{' associationFeatureList '}' ';'
+                        | '[' ASSOCIATION qualifierListEmpty ']' CLASS className alias '{' associationFeatureList '}' ';'
+                        | '[' ASSOCIATION qualifierListEmpty ']' CLASS className alias superClass '{' associationFeatureList '}' ';'
+                        """
+    
+def p_qualifierListEmpty(p):
+    """qualifierListEmpty : empty
+                          | qualifierListEmpty ',' qualifier
+                          """
+
+def p_associationFeatureList(p):
+    """associationFeatureList : empty 
+                              | associationFeatureList associationFeature
+                              """
+
+def p_indicDeclaration(p):
+    """indicDeclaration : '[' INDICATION qualifierListEmpty ']' CLASS className '{' classFeatureList '}' ';'
+                        | '[' INDICATION qualifierListEmpty ']' CLASS className superClass '{' classFeatureList '}' ';'
+                        | '[' INDICATION qualifierListEmpty ']' CLASS className alias '{' classFeatureList '}' ';'
+                        | '[' INDICATION qualifierListEmpty ']' CLASS className alias superClass '{' classFeatureList '}' ';'
+                        """
+
+def p_className(p):
+    """className : identifier"""
+
+def p_alias(p):
+    """alias : AS aliasIdentifier"""
+
+def p_aliasIdentifier(p):
+    """aliasIdentifier : '$' identifier"""
+
+def p_superClass(p):
+    """superClass : ':' className"""
+
+def p_classFeature(p):
+    """classFeature : propertyDeclaration
+                    | methodDeclaration
+                    | referenceDeclaration
+                    """
+
+def p_associationFeature(p):
+    """associationFeature : classFeature"""
+
+def p_qualifierList(p):
+    """qualifierList : '[' qualifier qualifierListEmpty ']'"""
+
+def p_qualifier(p): 
+    """qualifier : qualifierName
+                 | qualifierName ':' flavorList
+                 | qualifierName qualifierParameter
+                 | qualifierName qualifierParameter ':' flavorList
+                 """
+
+def p_flavorList(p):
+    """flavorList : flavor
+                  | flavorList flavor
+                  """
+
+def p_qualifierParameter(p):
+    """qualifierParameter : '(' constantValue ')'
+                          | arrayInitializer
+                          """
+
+def p_flavor(p):
+    """flavor : ENABLEOVERRIDE
+              | DISABLEOVERRIDE
+              | RESTRICTED
+              | TOSUBCLASS
+              | TRANSLATABLE
+              """
+
+def p_propertyDeclaration(p):
+    """propertyDeclaration : dataType propertyName ';'
+                           | dataType propertyName defaultValue ';'
+                           | dataType propertyName array ';'
+                           | dataType propertyName array defaultValue ';'
+                           | qualifierList dataType propertyName ';'
+                           | qualifierList dataType propertyName defaultValue ';'
+                           | qualifierList dataType propertyName array ';'
+                           | qualifierList dataType propertyName array defaultValue ';'
+                           """
+
+def p_referenceDeclaration(p):
+    """referenceDeclaration : objectRef referenceName ';'
+                            | objectRef referenceName defaultValue ';'
+                            | qualifierList objectRef referenceName ';'
+                            | qualifierList objectRef referenceName defaultValue ';'
+                            """
+
+def p_methodDeclaration(p):
+    """methodDeclaration : dataType methodName '(' ')' ';'
+                         | dataType methodName '(' parameterList ')' ';'
+                         | qualifierList dataType methodName '(' ')' ';'
+                         | qualifierList dataType methodName '(' parameterList ')' ';'
+                         """
+
+def p_propertyName(p):
+    """propertyName : identifier"""
+
+def p_referenceName(p):
+    """referenceName : identifier"""
+
+def p_methodName(p):
+    """methodName : identifier"""
+
+def p_dataType(p):
+    """dataType : DT_UINT8
+                | DT_SINT8
+                | DT_UINT16
+                | DT_SINT16
+                | DT_UINT32
+                | DT_SINT32
+                | DT_UINT64
+                | DT_SINT64
+                | DT_REAL32
+                | DT_REAL64
+                | DT_CHAR16
+                | DT_STR
+                | DT_BOOL
+                | DT_DATETIME
+                """
+
+def p_objectRef(p):
+    """objectRef : className REF"""
+
+def p_parameterList(p):
+    """parameterList : parameter
+                     | parameterList ',' parameter
+                     """
+def p_parameter(p):
+    """parameter : dataType parameterName
+                 | dataType parameterName array
+                 | qualifierList dataType parameterName
+                 | qualifierList dataType parameterName array
+                 | objectRef parameterName
+                 | objectRef parameterName array
+                 | qualifierList objectRef parameterName
+                 | qualifierList objectRef parameterName array
+                 """
+
+def p_parameterName(p):
+    """parameterName : identifier"""
+
+def p_array(p):
+    """array : '[' ']'
+             | '[' integerValue ']'
+             """
+
+def p_defaultValue(p):
+    """defaultValue : '=' initializer"""
+
+def p_initializer(p):
+    """initializer : constantValue
+                   | arrayInitializer
+                   | referenceInitializer"""
+
+def p_arrayInitializer(p):
+    """arrayInitializer : '{' constantValueList '}'
+                        | '{' '}'
+                        """
+
+def p_constantValueList(p):
+    """constantValueList : constantValue
+                         | constantValueList ',' constantValue
+                         """
+
+def p_stringValueList(p):
+    """stringValueList : stringValue
+                       | stringValueList stringValue
+                       """
+
+def p_constantValue(p):
+    """constantValue : integerValue
+                     | floatValue
+                     | charValue
+                     | stringValueList
+                     | booleanValue
+                     | nullValue
+                     """
+
+def p_integerValue(p):
+    """integerValue : binaryValue
+                    | octalValue
+                    | decimalValue
+                    | hexValue
+                    """
+
+def p_referenceInitializer(p):
+    """referenceInitializer : objectHandle
+                            | aliasIdentifier"""
+
+def p_objectHandle(p):
+    """objectHandle : identifier"""
+
+def p_qualifierDeclaration(p):
+    """qualifierDeclaration : QUALIFIER qualifierName qualifierType scope ';'
+                            | QUALIFIER qualifierName qualifierType scope defaultFlavor ';'
+                            | 
+                            """
+
+def p_qualifierName(p):
+    """qualifierName : identifier"""
+
+def p_qualifierType(p):
+    """qualifierType : ':' dataType
+                     | ':' dataType defaultValue
+                     | ':' dataType array
+                     | ':' dataType array defaultValue
+                     """
+
+def p_scope(p):
+    """scope : ',' SCOPE '(' metaElementList ')'"""
+
+def p_metaElementList(p):
+    """metaElementList : metaElement
+                       | metaElementList ',' metaElement
+                       """
+
+def p_metaElement(p):
+    """metaElement : SCHEMA
+                   | CLASS
+                   | ASSOCIATION
+                   | INDICATION
+                   | QUALIFIER
+                   | PROPERTY
+                   | REFERENCE
+                   | METHOD
+                   | PARAMETER
+                   | ANY
+                   """
+
+def p_defaultFlavor(p):
+    """defaultFlavor : ',' FLAVOR '(' flavorListWithComma ')'"""
+
+def p_flavorListWithComma(p):
+    """flavorListWithComma : flavor
+                           | flavorListWithComma ',' flavor
+                           """
+
+def p_instanceDeclaration(p):
+    """instanceDeclaration : INSTANCE OF className '{' valueInitializerList '}' ';'
+                           | INSTANCE OF className alias '{' valueInitializerList '}' ';'
+                           | qualifierList INSTANCE OF className '{' valueInitializerList '}' ';'
+                           | qualifierList INSTANCE OF className alias '{' valueInitializerList '}' ';'
+                           """
+
+def p_valueInitializerList(p):
+    """valueInitializerList : valueInitializer
+                            | valueInitializerList valueInitializer
+                            """
+
+def p_valueInitializer(p):
+    """valueInitializer : identifier defaultValue ';'
+                        | qualifierList identifier defaultValue ';'
+                        """
+
+def p_booleanValue(p):
+    """booleanValue : FALSE
+                    | TRUE
+                    """
+
+def p_nullValue(p):
+    """nullValue : NULL"""
+
+def p_identifier(p):
+    """identifier : IDENTIFIER
+                  | ANY
+                  | AS
+                  | ASSOCIATION
+                  | CLASS
+                  | DISABLEOVERRIDE
+                  | dataType 
+                  | ENABLEOVERRIDE
+                  | FLAVOR
+                  | INDICATION
+                  | INSTANCE
+                  | METHOD
+                  | OF
+                  | PARAMETER
+                  | PRAGMA
+                  | PROPERTY
+                  | QUALIFIER
+                  | REFERENCE
+                  | RESTRICTED
+                  | SCHEMA
+                  | SCOPE
+                  | TOSUBCLASS
+                  | TRANSLATABLE
+                  """
+
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_error(p):
+    print 'Syntax Error in input!'
+    print p
+    print 'column: ', find_column(mof, p)
+
+def find_column(input, token):
+    i = token.lexpos
+    while i > 0:
+        if input[i] == '\n':
+            break
+        i-= 1
+    column = (token.lexpos - i)+1
+    return column
+
+lexer = lex.lex(debug=1)
+yacc.yacc(debug=1)
 
 
 if __name__ == '__main__':
-    lex.runmain()
+    global mof
+    for w in os.walk('/usr/share/mof'):
+        for f in w[2]:
+            if not f.endswith('.mof'):
+                continue
+            file = '%s/%s' % (w[0], f)
+            print 'Parsing', file
+            mof = open(file, 'r').read()
+            lexer.lineno = 1
+            yacc.parse(mof)
+
